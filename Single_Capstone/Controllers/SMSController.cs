4a65667a0710703437a588d91c32a8a4b5e38117
Adam Nobs <nobsadam@gmail.com>
2020-02-03 14:53:44 -0600

    Twilio sms system is up and running. I need to tie it in to send the sms when Par level of inventory is below inventory levels

---
+++ Single_Capstone/Controllers/SMSController.cs
@@ -0,0 +1,28 @@
+﻿using Single_Capstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio;
+using Twilio.AspNet.Mvc;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace Single_Capstone.Controllers
+{
+    public class SMSController : TwilioController
+    {
+        // GET: SMS
+        public ActionResult SendSms()
+        {
+            TwilioClient.Init(PrivateKeys.AccountSID, PrivateKeys.AuthToken);
+
+            var message = MessageResource.Create(
+                body: "Join Earth's mightiest heroes. Like Kevin Bacon.",
+                from: new Twilio.Types.PhoneNumber(PrivateKeys.TwilioPhoneNumber),
+                to: new Twilio.Types.PhoneNumber(PrivateKeys.MyPhoneNumber)
+                );
+            return View();
+        }
+    }
+}
